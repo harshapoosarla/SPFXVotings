86cc9be3ae9cdafbffd4ec72b2d3a9e62ac322b5
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-10-27 17:14:37 +0530

    commit

---
+++ votings/src/webparts/votingsassignment/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVotingsassignmentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'VotingsassignmentWebPartStrings' {
+  const strings: IVotingsassignmentWebPartStrings;
+  export = strings;
+}
